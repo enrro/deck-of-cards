51bc5ccead3497d1fab19feed40d64d80e30a405
Israel Castro <israel.casgon@gmail.com>
2015-08-26 10:50:21 -0500

    card changed
    works

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -1,15 +1,14 @@
-
 public class DeckOfCards implements DeckOfCardsInterface {
 
-	private Card[] deck;	
+	public Card[] deck;	
 	private int position;
 	
 	public DeckOfCards(){
 		this.deck=new Card[52];
 		int num=0;
-		for(short j=0;j<4;j++){
-			for(short i=0;i<13;i++){
-				this.deck[num]=new Card(i,j);		//falta el hacer el constructor de la carta para aceptar los argumentos
+		for(short j=1;j<=4;j++){
+			for(short i=1;i<=13;i++){
+				this.deck[num]=new Card(i,j);
 				num++;
 			}
 		}
@@ -17,6 +16,7 @@ public class DeckOfCards implements DeckOfCardsInterface {
 	}
 	
 	public void shuffle() {
+		
 		int rnd;
 		Card temp;
 		for(int i=0;i<52;i++){
@@ -24,23 +24,45 @@ public class DeckOfCards implements DeckOfCardsInterface {
 			temp= this.deck[i];				//Guarda el valor actual del naipe en un temporal
 			this.deck[i]=this.deck[rnd];	//Guarda el valor de un lugar random de naipe en el lugar actual
 			this.deck[rnd]=temp;				//Guarda el temporal en el lugar random
+					
 		}
 	}
 
 	@Override
 	public int cardsLeft() {
-		return this.deck.length - this.position;
+		return position;
+		
 	}
 
 	@Override
 	public Card dealCard() {
-		this.position++;
-		return this.deck[this.position];
+		DeckOfCards dc = new DeckOfCards();
+		dc.shuffle();
+		
+		for (int count = 0 ; count < 1 ; count ++){
+			
+			String tmp = dc.deck[count].toString();
+			System.out.println(tmp);
+		}
+		return null;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		DeckOfCards dc = new DeckOfCards();
 
+		for (int count = 0 ; count < 52 ; count ++){
+			
+			String tmp = dc.deck[count].toString();
+			System.out.println(tmp);
+		}
+		dc.shuffle();
+		System.out.println("\n despues del shufle\n");
+		for (int count = 0 ; count < 52 ; count ++){
+			
+			String tmp = dc.deck[count].toString();
+			System.out.println(tmp);
+		}
 	}
 
 }
