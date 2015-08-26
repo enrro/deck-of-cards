0634ecc65489c898d4062d4158a56b33a9f58e2f
Israel Castro <israel.casgon@gmail.com>
2015-08-26 10:45:10 -0500

    Rigth Cases
    Added missing breaks

--- Card.java
+++ Card.java
@@ -18,26 +18,41 @@ public class Card {
 		String suit = "";
 		switch(this.rank){
 			case 1: rank= "Ace";
+			break;
 			case 2: rank= "Two";
+			break;
 			case 3: rank= "Three";
+			break;
 			case 4: rank="Four";
+			break;
 			case 5: rank= "Five";
+			break;
 			case 6: rank= "Six";
+			break;
 			case 7: rank= "Seven";
+			break;
 			case 8: rank= "Eight";
+			break;
 			case 9: rank= "Nine";
+			break;
 			case 10: rank= "Ten";
+			break;
 			case 11: rank= "Jack";
+			break;
 			case 12: rank= "Queen";
+			break;
 			case 13: rank= "King";
+			break;
 		}
 		switch(this.suit){
 			case 1: suit= "Clubs";
+			break;
 			case 2: suit= "Diamonds";
+			break;
 			case 3: suit= "Hearts";
+			break;
 			case 4: suit= "Spades";
-			case 5: suit= "rofl";
-			case 6: suit= "stack";
+			break;
 		}
 		return rank+" of "+suit;
 	}
